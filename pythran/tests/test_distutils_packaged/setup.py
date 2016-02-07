87735c858644608efd11a8af5eec753d00b55735
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2016-02-07 13:15:25 +0100

    Better option handling in PythranExtension

--- pythran/tests/test_distutils_packaged/setup.py
+++ pythran/tests/test_distutils_packaged/setup.py
@@ -2,7 +2,6 @@ from distutils.core import setup, Extension
 from pythran import PythranExtension
 
 module1 = PythranExtension('demo2.a', sources = ['a.py'])
-
 setup(name = 'demo2',
       version = '1.0',
       description = 'This is another demo package',
